6024a6a4dffbf914535ca5641dc71b4ad782f8d3
Amir Golparvar <golparva>
2021-08-30 15:51:38 +0200

    second

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 #!/usr/bin/env node
 
-console.log('hi!');
+const chokidar = require ('chokidar');
+const debounce = require ('lodash.debounce');
+const program = require('caporal');
+const fs = require('fs');
+const { spawn } = require('child_process');
+
+program
+  .version('0.0.1')
+  .argument('[filename]', 'name of the file to execute')
+  .action(async ({ filename }) => {
+    const name = filename || "index.js";
+
+    try {
+        await fs.promises.access(name);
+    } catch (err) {
+        throw new Error(`could not find the file ${name}`);
+    }
+
+    const start = debounce(() => {
+        spawn('node', [name], { stdio: 'inherit' });
+        // console.log('STARTING USERS PROGRAM');
+    }, 100);
+    
+    chokidar.watch('.')
+      .on('add', start)
+      .on('change', start)
+      .on('unlink', start);
+    
+  });
+
+program.parse(process.argv);
+
+
